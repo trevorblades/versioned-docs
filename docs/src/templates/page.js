4a01840a8f700c08c120bd73c5a4396915bcef94
Trevor Blades <tdblades@gmail.com>
2021-10-10 22:30:49 -0400

    Split relativelink out

--- docs/src/templates/page.js
+++ docs/src/templates/page.js
@@ -1,29 +1,11 @@
 import PropTypes from 'prop-types';
-import React, {createContext, useContext} from 'react';
+import React from 'react';
+import RelativeLink, {PathContext} from '../components/RelativeLink';
 import VersionDropdown from '../components/VersionDropdown';
 import {Helmet} from 'react-helmet';
-import {Link, graphql} from 'gatsby';
 import {MDXProvider} from '@mdx-js/react';
 import {MDXRenderer} from 'gatsby-plugin-mdx';
-import {isAbsolute, resolve} from 'path-browserify';
-
-function RelativeLink({href, ...props}) {
-  const path = useContext(PathContext);
-  try {
-    const url = new URL(href);
-    return <a href={url.toString()} {...props} />;
-  } catch (error) {
-    return (
-      <Link to={isAbsolute(href) ? href : resolve(path, href)} {...props} />
-    );
-  }
-}
-
-RelativeLink.propTypes = {
-  href: PropTypes.string.isRequired
-};
-
-const PathContext = createContext();
+import {graphql} from 'gatsby';
 
 export default function PageTemplate({data, path}) {
   const {body, frontmatter, parent} = data.mdx;
